92bca9ab8244b16642b80b3a3df18fa686fe8dd2
KavitaRJadhav <jadhav.kavita7881@gmail.com>
2024-02-07 11:40:39 +0530

    Initial commit

---
+++ 07-Feb-2024/Lab001.py
@@ -0,0 +1,15 @@
+# Boolean
+# True False
+
+is_jay_married = False
+Var_Jay = 16
+
+# list is unordered collection of items
+my_list = [1, 7, 5, 3, 7, 6]
+print(my_list)
+
+# Set is unordered collection of unique items
+my_set = {1, 2, 8, 5, 4, 4,}
+print(my_set)
+print(type(my_list))
+print(type(my_set))
