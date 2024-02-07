bc2a6f774ef5026fb60acf573740d5ddbc214676
KavitaRJadhav <jadhav.kavita7881@gmail.com>
2024-02-07 11:48:17 +0530

    Hi Lab001 updated

--- 07-Feb-2024/Lab001.py
+++ 07-Feb-2024/Lab001.py
@@ -6,10 +6,14 @@ Var_Jay = 16
 
 # list is unordered collection of items
 my_list = [1, 7, 5, 3, 7, 6]
+my_list1 = ['a', 'b', 'd', 'c', 'g', 'h']
 print(my_list)
+print(my_list1)
 
 # Set is unordered collection of unique items
 my_set = {1, 2, 8, 5, 4, 4,}
+my_set1 = {'a', 'n', 'e', 'd', 'n', 'f'}
 print(my_set)
+print(my_set1)
 print(type(my_list))
 print(type(my_set))
